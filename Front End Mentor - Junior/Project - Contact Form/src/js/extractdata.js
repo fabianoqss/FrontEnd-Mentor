d0aac941548cf779ef2ac17810e374113e69cb1e
Fabiano <fabianoquirinos@outlook.com>
2025-01-27 09:45:08 -0300

    :recycle: refactor: Refatorando para funções separadas.

--- Front End Mentor - Junior/Project - Contact Form/src/js/extractdata.js
+++ Front End Mentor - Junior/Project - Contact Form/src/js/extractdata.js
@@ -1,33 +1,44 @@
 const forms = document.getElementById('forms');
+const successMessage = document.getElementById('success-message');
 
-function handleChange(event) {
-  const target = event.target;
-  if (!target.checkValidity()) {
-    target.classList.add('border-borderError');
-    target.setCustomValidity('This field is Required');
-    target.nextElementSibling.innerText = target.validationMessage;
-    event.preventDefault();
-  } else {
-    target.classList.remove('border-borderError');
-  }
+function showError(target, message) {
+  target.classList.add('border-borderError');
+  target.setCustomValidity(message);
+  target.nextElementSibling.innerText = target.validationMessage;
+}
 
+function clearError(target) {
+  target.classList.remove('border-borderError');
+  target.setCustomValidity('');
+  target.nextElementSibling.innerText = '';
+}
+
+function saveFormData(target) {
   const formData = JSON.parse(localStorage.getItem('formData')) || {};
   formData[target.name] = target.value;
   localStorage.setItem('formData', JSON.stringify(formData));
 }
 
+function handleChange(event) {
+  const target = event.target;
+  if (!target.checkValidity()) {
+    showError(target, 'This field is Required');
+    event.preventDefault();
+  } else {
+    clearError(target);
+  }
+  saveFormData(target);
+}
+
 function successful(event) {
   event.preventDefault();
-  const success = document.getElementById('success-message');
-  success.classList.remove('hidden');
+  successMessage.classList.remove('hidden');
+
+  setTimeout(() => successMessage.classList.add('hidden'), 3000);
 
   setTimeout(() => {
-    success.classList.add('hidden');
-  }, 3000);
-
-  setTimeout(() => {
+    forms.reset();
     window.location.reload();
-    formulario.reset();
   }, 5000);
 }
 
