342ce34ea95d4eaaf6a742c80cb06f2e06401602
Fabiano <fabianoquirinos@outlook.com>
2025-02-05 09:15:08 -0300

    :broom: cleanup: Limpeza no Código

--- Front End Mentor - Junior/Project - Contact Form/src/js/extractdata.js
+++ Front End Mentor - Junior/Project - Contact Form/src/js/extractdata.js
@@ -1,8 +1,21 @@
-const formulario = document.querySelector('#forms');
+const formulario = document.getElementById('forms');
 
-function envioFormulario(e) {
-  const target = e.target;
-  console.log(target);
+function submitForms(e) {
+  e.preventDefault();
+
+  const inputs = formulario.querySelectorAll('input[required]');
+
+  inputs.forEach((input) => {
+    const span = input.nextElementSibling;
+
+    if (!input.checkValidity()) {
+      span.innerText = 'This field is required';
+      span.classList.remove('hidden');
+    } else {
+      span.innerText = '';
+      span.classList.add('hidden');
+    }
+  });
 }
 
-formulario.addEventListener('click', envioFormulario);
+formulario.addEventListener('submit', submitForms);
